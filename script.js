3f897a4fda34a44c0c91a7f94c25ad7ae59b06f6
Adriana <a.s.m27267@gmail.com>
2022-07-17 19:53:50 -0300

    trabalhando com api

--- script.js
+++ script.js
@@ -1,16 +1,82 @@
-const responce = fetch('https://jsonplaceholder.typicode.com/users')
+const getUsers = () => {
+  const responce = fetch('https://jsonplaceholder.typicode.com/users')
+    .then(responce => {
+      return responce.json()
+    })
+    .then(data => {
+      const users = data;
+      var tbody = document.querySelector('#tbody');
+      users.forEach(u => {
+        var tr = document.createElement('tr');
+        tbody.appendChild(tr);
 
-.then(res => {
-    console.log(res)
-    return res.json()
-})
-.then(data  => {
-  data.map(user=> {
-    console.log(`# Nome: ${user.id}, Nome de Usuário: ${user.username}, Email: ${user.email},  Endereço: ${user.street}` )
-    
+        var th = document.createElement('th');
+        tr.appendChild(th);
+        th.textContent = u.id
+
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = u.name
+
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = u.username
+
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = u.email
+
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = u.address.street
+
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = u.company.name
+
+      });
+      console.log(data)
+    });
+}
+getUsers();
+
+const getuser =()=>{
+  const id = document.getElementById('idUser').value;
+  console.log()
+  fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+  .then(response=>{
+    return response.json()
+  })
+  .then(data=>{
+    console.log(data)
+  });
+}
+
+const addPost = () => {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo',
+      body: ' bar',
+      userId: 1,
+    }),
+    headers: {
+      'content-type': 'aplicattion/json; charset=UTF-8'
+    },
   })
 
-})
-  
-console.log(responce);   
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json)
+      getUser(json.id)
+    });
+}
+const getUser = id => {
+  fetch(` https://jsonplaceholder.typicode.com/posts ${id}`)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+}
+
+addPost();
 
